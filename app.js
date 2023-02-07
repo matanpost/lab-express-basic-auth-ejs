6c105073b070700034f1c86e54773858352649d0
Matan Post <matanpost@gmail.com>
2023-02-07 16:13:38 +0100

    middle of iteration 2

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.use('/', index)
 const auth = require('./routes/auth')
 app.use('/auth', auth)
 
+const login = require('./routes/auth')
+app.use('/auth', login)
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
--- routes/auth.js
+++ routes/auth.js
@@ -9,22 +9,56 @@ router.get("/signup", (req, res, next) => {
 
 /* POST signup page */
 router.post("/signup", async(req, res, next) => {
+   const body ={...req.body }
+
+   if (body.password.length < 6) {
+    res.render('auth/signup', { errorMessage: 'Password too short', body: req.body })
+  } else{
+
+      const bcrypt = require('bcryptjs');   
+      const salt = bcrypt.genSaltSync(12);
+      const passwordHash = bcrypt.hashSync("body.password", salt);
+      console.log(passwordHash)
+      body.password = passwordHash;
   
-    const body ={...req.body }
+      try{
+         await User.create(body);
+         res.send(body);
+      }
+      catch(error){
+          console.log("error in auth promise", error);
+      };
+  }
+})
 
-    const bcrypt = require('bcryptjs');   
-    const salt = bcrypt.genSaltSync(12);
-    const passwordHash = bcrypt.hashSync("body.password", salt);
-    console.log(passwordHash)
-    body.password = passwordHash;
+// Get LOGIN page
+router.get("/login", (req, res, next) => {
+    res.render('login')
+})
 
-    try{
-       await User.create(body);
-       res.send(body);
+/* POST signup page */
+router.post("/login", async(req, res, next) => {
+    console.log('SESSION =====> ', req.session)
+    const body = req.body
+  
+    const userMatch = await User.find({ username: body.username })
+    console.log(userMatch)
+    if (userMatch.length) {
+      // User found
+      const user = userMatch[0]
+  
+      if (bcrypt.compareSync(body.password, user.passwordHash)) {
+        // Correct password
+        console.log(user)
+        res.render('profile', { user })
+      } else {
+        // Incorrect password
+      }
+    } else {
+      // User not found
     }
-    catch(error){
-        console.log("error in auth promise", error);
-    };
+
+    
 })
 
 module.exports = router;
